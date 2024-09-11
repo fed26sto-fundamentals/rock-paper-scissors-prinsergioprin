aa78459a65b28cd77da1c415a490f1d1e98095a1
prinsergioprin <sergioprin24@gmail.com>
2024-09-11 12:13:30 +0200

    Write the logic to get the human choice

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const choices = ['rock', 'paper', 'scissors']
 function getComputerChoice() {
     return choices [Math.floor(Math.random()*choices.length)];
 }
+
+function getHumanChoice() {
+    let input = prompt('Type Rock, Paper, or Scissors');
+    input = input.toLowerCase();
+}
